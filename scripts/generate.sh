0a0604d297783ba40021df39520ecbdfae5cde5f
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-09-21 14:16:39 +0300

    fix: directory

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -14,14 +14,9 @@ done
 
 java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l php -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
 
-sed -i '' 's/"description": ""/"description": "Fingerprint Pro Server API provides a way for validating visitors’ data issued by Fingerprint Pro."/g' sdk/composer.json
-sed -i '' 's/"homepage": "http:\/\/swagger.io"/"homepage": "https:\/\/www.fingerprint.com"/g' sdk/composer.json
-sed -i '' 's/"license": "proprietary",/"license": "MIT",/g' sdk/composer.json
-sed -i '' 's/"name": "Swagger and contributors",/"name": "fingerprintjs",/g' sdk/composer.json
-sed -i '' 's/"homepage": "https:\/\/github.com\/swagger-api\/swagger-codegen"/"homepage": "https:\/\/github.com\/fingerprintjs\/fingerprint-pro-server-api-php-sdk"/g' sdk/composer.json
-
 mv -f sdk/README.md ./README.md
+rm -rf sdk/composer.json
 rm -rf docs
 mv -f sdk/docs ./docs
 rm -rf sdk/test
-rm sdk/{git_push.sh,.php_cs,.travis.yml, phpunit.xml.dist}
+rm sdk/{git_push.sh,.php_cs,.travis.yml,phpunit.xml.dist}
