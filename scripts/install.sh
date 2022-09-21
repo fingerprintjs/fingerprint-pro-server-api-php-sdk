2299fe5c057d940a0b91d6eb2e3460cbabfe90a7
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-09-21 14:32:15 +0300

    feat: change composer to setup-php with tools

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,1 @@
-docker-compose run composer install --ignore-platform-reqs
+docker-compose run composer install --profile --ignore-platform-reqs --no-interaction --no-ansi --no-scripts --no-suggest --prefer-dist
