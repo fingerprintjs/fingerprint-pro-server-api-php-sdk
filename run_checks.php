4ee969192b379b1dcd1fc1d8d1a56b8a1f678ae2
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-10-03 11:35:07 +0300

    feat(MINOR): add functional tests

---
+++ run_checks.php
@@ -0,0 +1,33 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+$host = getenv('FP_API_HOST');
+$api_key = getenv('FP_PRIVATE_API_KEY');
+
+$config = Fingerprintjs\ServerAPI\Configuration::getDefaultConfiguration()
+    ->setApiKey('api_key', $api_key)
+    ->setHost($host);
+
+$apiInstance = new Fingerprintjs\ServerAPI\Api\FingerprintApi(
+    new GuzzleHttp\Client(),
+    $config
+);
+
+$visitor_id = getenv('FP_VISITOR_ID');
+$request_id = getenv('FP_REQUEST_ID');
+
+try {
+    $result = $apiInstance->getVisits($visitor_id);
+} catch (Exception $e) {
+    fwrite(STDERR, sprintf("Exception when calling FingerprintApi->getVisits: %s\n", $e->getMessage()));
+    exit(1);
+}
+
+try {
+    $result = $apiInstance->getEvent($request_id);
+} catch (Exception $e) {
+    fwrite(STDERR, sprintf("Exception when calling FingerprintApi->getVisits: %s\n", $e->getMessage()));
+    exit(1);
+}
+
+fwrite(STDOUT, "Checks passed\n");
+exit(0);
