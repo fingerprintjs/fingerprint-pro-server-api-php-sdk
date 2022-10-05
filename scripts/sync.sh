e3761836d97c9d82cb37fd09df110426aae5a3a1
Orkun <orkuncakilkaya@yandex.com.tr>
2022-10-05 11:22:20 +0300

    feat(MINOR): add contributing guideline & sync (#27)

---
+++ scripts/sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -o ./res/fingerprint-server-api.yaml https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/schemes/fingerprint-server-api.yaml
+
+examplesList=(
+  'visits_limit_500.json'
+  'get_event.json'
+)
+
+for example in ${examplesList[*]}; do
+  curl -o ./test/mocks/"$example" https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/examples/"$example"
+done
+
+./scripts/generate.sh
