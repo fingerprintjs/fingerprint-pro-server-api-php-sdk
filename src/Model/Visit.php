e285f3388b085a1b5cdc462bd793e2f676c2d0dc
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-10-19 10:59:54 +0300

    feat: update schema to support url field for botd result

--- src/Model/Visit.php
+++ src/Model/Visit.php
@@ -257,7 +257,7 @@ class Visit implements ModelInterface, ArrayAccess
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = [];
+        $invalidProperties = parent::listInvalidProperties();
 
         if ($this->container['request_id'] === null) {
             $invalidProperties[] = "'request_id' can't be null";
