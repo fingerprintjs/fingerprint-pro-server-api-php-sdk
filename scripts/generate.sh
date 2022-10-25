9a95a75e6e5ac1857dc54c3b3b2ce9f168956e08
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-10-25 13:53:31 +0300

    fix: updated list of ignored generated files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,7 +12,7 @@ done
 
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
-sed -i "s/\"artifactVersion\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
+sed -i '' "s/\"artifactVersion\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
 
 java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l php -i ./res/fingerprint-server-api.yaml -o ./ -c config.json
 
@@ -21,4 +21,3 @@ mv -f src/composer.json composer.json
 rm -rf docs
 mv -f src/docs ./docs
 rm -rf src/test
-rm -rf src/{git_push.sh,.php_cs,.travis.yml,phpunit.xml.dist}
