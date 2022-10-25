8fced34b4157c66add3662148fc8b03adafdea6f
Orkun <orkuncakilkaya@yandex.com.tr>
2022-10-25 15:57:15 +0300

    fix: updated list of ignored generated files (#37)
    * fix: updated list of ignored generated files
    
    * chore: fix generate script
    
    * chore: fix generate script
    
    * chore: remove tests from swagger-codegen

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,7 +12,16 @@ done
 
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
-sed -i "s/\"artifactVersion\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
+if [[ $VERSION == *"develop"* ]]; then
+  SANITIZED_VERSION=$(sed 's/-develop//g' <<< $VERSION)
+  SANITIZED_VERSION=$(sed 's/\.[0-9]*$//g' <<< $SANITIZED_VERSION)
+  BUILD_VERSION=$(grep -o '[0-9]*$' <<< $VERSION)
+  VERSION="dev-$SANITIZED_VERSION-$BUILD_VERSION"
+fi
+
+echo "VERSION: $VERSION"
+
+sed -i "s/\"artifactVersion\": \".*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
 
 java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l php -i ./res/fingerprint-server-api.yaml -o ./ -c config.json
 
@@ -20,5 +29,3 @@ mv -f src/README.md ./README.md
 mv -f src/composer.json composer.json
 rm -rf docs
 mv -f src/docs ./docs
-rm -rf src/test
-rm -rf src/{git_push.sh,.php_cs,.travis.yml,phpunit.xml.dist}
