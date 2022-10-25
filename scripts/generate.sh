c3bf774d251c343b92b08b7936d2a60328879de0
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-10-25 14:33:08 +0300

    chore: fix generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,7 +12,7 @@ done
 
 # jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
 
-sed -i '' "s/\"artifactVersion\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
+sed -i "s/\"artifactVersion\": \"[0-9]*\.[0-9]*\.[0-9]*\"/\"artifactVersion\": \"$VERSION\"/g" config.json
 
 java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l php -i ./res/fingerprint-server-api.yaml -o ./ -c config.json
 
