89b0549aacf64c6d2339c11d1ac7fd0695ddc6c6
Juraj Uhlar <juro.uhlar@gmail.com>
2023-01-18 10:16:25 +0000

    chore: update url in sync.sh to 'schemas'

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o ./res/fingerprint-server-api.yaml https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/schemes/fingerprint-server-api.yaml
+curl -o ./res/fingerprint-server-api.yaml https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/schemas/fingerprint-server-api.yaml
 
 examplesList=(
   'visits_limit_500.json'
