c7d862f395b64b18c54bb6d5b79043888c00fee3
Juraj Uhlar <juro.uhlar@gmail.com>
2023-07-24 14:10:59 +0100

    chore: improve local testing dx (run_checks.php, contributing.md)

--- run_checks.php
+++ run_checks.php
@@ -1,33 +1,58 @@
 <?php
 require_once(__DIR__ . '/vendor/autoload.php');
-$host = getenv('FP_API_HOST');
-$api_key = getenv('FP_PRIVATE_API_KEY');
 
-$config = Fingerprint\ServerAPI\Configuration::getDefaultConfiguration()
-    ->setApiKey('api_key', $api_key)
-    ->setHost($host);
+use Fingerprint\ServerAPI\Api\FingerprintApi;
+use Fingerprint\ServerAPI\Configuration;
+use GuzzleHttp\Client;
 
-$apiInstance = new Fingerprint\ServerAPI\Api\FingerprintApi(
-    new GuzzleHttp\Client(),
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$api_key = $_ENV['FP_PRIVATE_API_KEY'];
+$visitor_id = $_ENV['FP_VISITOR_ID'];
+$request_id = $_ENV['FP_REQUEST_ID'];
+$region_env = $_ENV['FP_REGION'] ?? "us";
+
+$region = Configuration::REGION_GLOBAL;
+if ($region_env === 'eu') {
+    $region = Configuration::REGION_EUROPE;
+} else if ($region_env === 'ap') {
+    $region = Configuration::REGION_ASIA;
+}
+
+$config = Configuration::getDefaultConfiguration(
+    $api_key,
+    $region,
+);
+
+$client = new FingerprintApi(
+    new Client(),
     $config
 );
 
-$visitor_id = getenv('FP_VISITOR_ID');
-$request_id = getenv('FP_REQUEST_ID');
+// Temporarily suppress a million deprecated ArrayAccess return type warnings for readability
+// Our SDK generator does not yet support PHP's new attributes system
+// https://github.com/swagger-api/swagger-codegen/issues/11820
+error_reporting(error_reporting() & ~E_DEPRECATED);
 
 try {
-    $result = $apiInstance->getVisits($visitor_id);
+    $result = $client->getVisits($visitor_id);
+    fwrite(STDOUT, sprintf("Got visits: %s \n", $result));
 } catch (Exception $e) {
     fwrite(STDERR, sprintf("Exception when calling FingerprintApi->getVisits: %s\n", $e->getMessage()));
     exit(1);
 }
 
 try {
-    $result = $apiInstance->getEvent($request_id);
+    $result = $client->getEvent($request_id);
+    fwrite(STDOUT, sprintf("Got event: %s \n", $result));
 } catch (Exception $e) {
     fwrite(STDERR, sprintf("Exception when calling FingerprintApi->getVisits: %s\n", $e->getMessage()));
     exit(1);
 }
+
+// Enable the deprecated ArrayAccess return type warning again if needed
+error_reporting(error_reporting() | E_DEPRECATED);
 
 fwrite(STDOUT, "Checks passed\n");
 exit(0);
