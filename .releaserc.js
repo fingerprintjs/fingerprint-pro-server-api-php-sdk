98c02272f50874ea9b90ba996a8298e6e04d1096
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-03-27 18:21:30 +0900

    ci: use shared release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    
+
     "branches": [
         "main",
         {
@@ -11,93 +11,14 @@ module.exports = {
         [
             "@semantic-release/commit-analyzer",
             {
-                "releaseRules": [
-                    {
-                        "type": "build",
-                        "scope": "deps",
-                        "release": "patch"
-                    },
-                    {
-                        "type": "docs",
-                        "scope": "README",
-                        "release": "patch"
-                    }
-                ]
+                "config": "@fingerprintjs/conventional-changelog-dx-team",
+                "releaseRules": "@fingerprintjs/conventional-changelog-dx-team/release-rules"
             }
         ],
         [
             "@semantic-release/release-notes-generator",
             {
-                "preset": "conventionalCommits",
-                "presetConfig": {
-                    "types": [
-                        {
-                            "type": "feat",
-                            "section": "Features"
-                        },
-                        {
-                            "type": "feature",
-                            "section": "Features"
-                        },
-                        {
-                            "type": "fix",
-                            "section": "Bug Fixes"
-                        },
-                        {
-                            "type": "perf",
-                            "section": "Performance Improvements"
-                        },
-                        {
-                            "type": "revert",
-                            "section": "Reverts"
-                        },
-                        {
-                            "type": "docs",
-                            "scope": "README",
-                            "section": "Documentation"
-                        },
-                        {
-                            "type": "build",
-                            "scope": "deps",
-                            "section": "Build System"
-                        },
-                        {
-                            "type": "docs",
-                            "section": "Documentation",
-                            "hidden": true
-                        },
-                        {
-                            "type": "style",
-                            "section": "Styles",
-                            "hidden": true
-                        },
-                        {
-                            "type": "chore",
-                            "section": "Miscellaneous Chores",
-                            "hidden": true
-                        },
-                        {
-                            "type": "refactor",
-                            "section": "Code Refactoring",
-                            "hidden": true
-                        },
-                        {
-                            "type": "test",
-                            "section": "Tests",
-                            "hidden": true
-                        },
-                        {
-                            "type": "build",
-                            "section": "Build System",
-                            "hidden": true
-                        },
-                        {
-                            "type": "ci",
-                            "section": "Continuous Integration",
-                            "hidden": true
-                        }
-                    ]
-                }
+                "config": "@fingerprintjs/conventional-changelog-dx-team"
             }
         ],
         [
