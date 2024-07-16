8989f57f81cfab050d49ce6414527401f403dfee
Orkun <orkuncakilkaya@gmail.com>
2024-07-16 16:47:04 +0300

    chore(generator): update raw response models to an array

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -69,20 +69,15 @@ find ./docs -type f ! -name "DecryptionKey.md" ! -name "Sealed.md" -exec rm {} +
 mv -f src/docs/* ./docs
 cp template/docs/FingerprintApi.md docs/Api
 
-# enable WithRawResponse trait for EventResponse model
-(
-  if [ "$platform" = "Darwin" ]; then
-    sed -i '' 's/\/\/ use \\/use \\/' ./src/Model/EventResponse.php
-  else
-    sed -i 's/\/\/ use \\/use \\/' ./src/Model/EventResponse.php
-  fi
-)
+declare -a modelsWithRawResponse=("./src/Model/EventResponse.php" "./src/Model/Response.php")
 
-# enable WithRawResponse trait for Response model
-(
-  if [ "$platform" = "Darwin" ]; then
-    sed -i '' 's/\/\/ use \\/use \\/' ./src/Model/Response.php
-  else
-    sed -i 's/\/\/ use \\/use \\/' ./src/Model/Response.php
-  fi
-)
+for model in "${modelsWithRawResponse[@]}"; do
+  # enable WithRawResponse trait for the model
+  (
+    if [ "$platform" = "Darwin" ]; then
+      sed -i '' 's/\/\/ use \\/use \\/' $model
+    else
+      sed -i 's/\/\/ use \\/use \\/' $model
+    fi
+  )
+done
