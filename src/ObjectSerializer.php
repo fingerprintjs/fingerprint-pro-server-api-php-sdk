5b821b65ce75a3fad9dead5710181ac9d52cf6ad
Orkun <orkuncakilkaya@gmail.com>
2024-08-12 12:59:52 +0300

    refactor: remove redundant methods from object serializer

--- src/ObjectSerializer.php
+++ src/ObjectSerializer.php
@@ -29,7 +29,6 @@
 
 namespace Fingerprint\ServerAPI;
 
-use DateTime;
 use Psr\Http\Message\ResponseInterface;
 
 /**
@@ -99,23 +98,6 @@ class ObjectSerializer
     }
 
     /**
-     * Sanitize filename by removing path.
-     * e.g. ../../sun.gif becomes sun.gif.
-     *
-     * @param string $filename filename to be sanitized
-     *
-     * @return string the sanitized filename
-     */
-    public static function sanitizeFilename(string $filename): string
-    {
-        if (preg_match('/.*[\/\\\](.*)$/', $filename, $match)) {
-            return $match[1];
-        }
-
-        return $filename;
-    }
-
-    /**
      * Take value and turn it into a string suitable for inclusion in
      * the path, by url-encoding.
      *
@@ -150,38 +132,6 @@ class ObjectSerializer
 
     /**
      * Take value and turn it into a string suitable for inclusion in
-     * the header. If it's a string, pass through unchanged
-     * If it's a datetime object, format it in RFC3339.
-     *
-     * @param string $value a string which will be part of the header
-     *
-     * @return string the header string
-     */
-    public static function toHeaderValue(string $value): string
-    {
-        return self::toString($value);
-    }
-
-    /**
-     * Take value and turn it into a string suitable for inclusion in
-     * the http body (form parameter). If it's a string, pass through unchanged
-     * If it's a datetime object, format it in RFC3339.
-     *
-     * @param \SplFileObject|string $value the value of the form parameter
-     *
-     * @return string the form string
-     */
-    public static function toFormValue(\SplFileObject|string $value): string
-    {
-        if ($value instanceof \SplFileObject) {
-            return $value->getRealPath();
-        }
-
-        return self::toString($value);
-    }
-
-    /**
-     * Take value and turn it into a string suitable for inclusion in
      * the parameter. If it's a string, pass through unchanged
      * If it's a datetime object, format it in RFC3339
      * If it's a date, format it in Y-m-d.
@@ -198,30 +148,6 @@ class ObjectSerializer
         }
 
         return $value;
-    }
-
-    /**
-     * Serialize an array to a string.
-     *
-     * @param array  $collection                 collection to serialize to a string
-     * @param string $collectionFormat           the format use for serialization (csv,
-     *                                           ssv, tsv, pipes, multi)
-     * @param bool   $allowCollectionFormatMulti allow collection format to be a multidimensional array
-     */
-    public static function serializeCollection(array $collection, string $collectionFormat, ?bool $allowCollectionFormatMulti = false): string
-    {
-        if ($allowCollectionFormatMulti && ('multi' === $collectionFormat)) {
-            // http_build_query() almost does the job for us. We just
-            // need to fix the result of multidimensional arrays.
-            return preg_replace('/%5B[0-9]+%5D=/', '=', http_build_query($collection, '', '&'));
-        }
-
-        return match ($collectionFormat) {
-            'pipes' => implode('|', $collection),
-            'tsv' => implode("\t", $collection),
-            'ssv' => implode(' ', $collection),
-            default => implode(',', $collection),
-        };
     }
 
     /**
