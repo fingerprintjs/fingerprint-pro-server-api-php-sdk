8daa0562c6b4cb3685488b32ae2d8a6fb0b88414
Orkun <orkuncakilkaya@gmail.com>
2024-09-03 13:49:19 +0300

    chore(coverage-diff): prettify overall coverage table

--- coverage.php
+++ coverage.php
@@ -7,35 +7,60 @@ $contents = file_get_contents(__DIR__ . "/cov/xml/clover.xml");
 $xml = simplexml_load_string($contents);
 $arr = json_decode(json_encode($xml), true);
 
-// Calculate total coverage
-$elements = (int)$arr["project"]["metrics"]["@attributes"]["elements"];
-$covered = (int)$arr["project"]["metrics"]["@attributes"]["coveredelements"];
-$percentage = ($covered / $elements) * 100;
+$metrics = $arr["project"]["metrics"]["@attributes"];
+
+// Calculate coverage metrics
+$statementsCovered = (int)$metrics["coveredstatements"];
+$statementsTotal = (int)$metrics["statements"];
+$statementsPct = $statementsTotal > 0 ? ($statementsCovered / $statementsTotal) * 100 : 0;
+
+$functionsCovered = (int)$metrics["coveredmethods"];
+$functionsTotal = (int)$metrics["methods"];
+$functionsPct = $functionsTotal > 0 ? ($functionsCovered / $functionsTotal) * 100 : 0;
+
+// Function to return color based on percentage
+function getCoverageStatus($percentage) {
+    if ($percentage >= 80) {
+        return ":green_circle:";
+    } elseif ($percentage >= 50) {
+        return ":yellow_circle:";
+    } else {
+        return ":red_circle:";
+    }
+}
 
 // Generate JSON report
 $map = [
     "total" => [
-        "statements" => ["pct" => $percentage],
+        "statements" => ["pct" => $statementsPct],
     ]
 ];
 file_put_contents($outputPath . "index.json", json_encode($map));
 
 // Generate Markdown report
 $markdown = "# Code Coverage Report\n\n";
-$markdown .= "## Overall Coverage\n\n";
-$markdown .= "| Lines  | Covered | Coverage |\n";
-$markdown .= "|--------|---------|----------|\n";
+$markdown .= "## Coverage Report\n\n";
+$markdown .= "| St. | Category       | Percentage | Covered / Total |\n";
+$markdown .= "|-----|----------------|------------|-----------------|\n";
 $markdown .= sprintf(
-    "| %d | %d | %.2f%% |\n",
-    $elements,
-    $covered,
-    $percentage
+    "| %s | Statements      | %.2f%%     | %d / %d          |\n",
+    getCoverageStatus($statementsPct),
+    $statementsPct,
+    $statementsCovered,
+    $statementsTotal
+);
+$markdown .= sprintf(
+    "| %s | Functions       | %.2f%%     | %d / %d          |\n",
+    getCoverageStatus($functionsPct),
+    $functionsPct,
+    $functionsCovered,
+    $functionsTotal
 );
 
 $markdown .= "\n<details>\n";
 $markdown .= "<summary>Files Coverage</summary>\n\n";
 $markdown .= "| File | Methods | Statements | Total Coverage |\n";
-$markdown .= "|------|-------------------|------------------------|----------------|\n";
+$markdown .= "|------|---------|------------|----------------|\n";
 
 foreach ($arr["project"]["file"] as $file) {
     $filePath = 'src/' . explode('src/', $file["@attributes"]['name'])[1];
