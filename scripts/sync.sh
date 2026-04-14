49393d66f07038cc9e1f254142cfe801940bfe4d
Eray Aydın <eray.aydin@fingerprint.com>
2026-04-14 20:35:32 +0300

    ci: add missing mock example files, re-order sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -21,24 +21,23 @@ curl "${CURL_OPTS[@]}" -o ./res/fingerprint-server-api.yaml "$schemaUrl"
 examples=(
   'events/get_event_200.json'
   'events/get_event_ruleset_200.json'
-  'events/get_event_with_bot_info_200.json'
   'events/search/get_event_search_200.json'
-  'events/update_event_multiple_fields_request.json'
-  'events/update_event_one_field_request.json'
   'webhook/webhook_event.json'
   'errors/400_event_id_invalid.json'
   'errors/400_request_body_invalid.json'
+  'errors/400_ruleset_not_found.json'
   'errors/400_visitor_id_invalid.json'
   'errors/400_visitor_id_required.json'
   'errors/403_feature_not_enabled.json'
   'errors/403_secret_api_key_not_found.json'
   'errors/403_secret_api_key_required.json'
-  'errors/403_wrong_region.json'
   'errors/403_subscription_not_active.json'
+  'errors/403_wrong_region.json'
   'errors/404_event_not_found.json'
   'errors/404_visitor_not_found.json'
   'errors/409_state_not_ready.json'
   'errors/429_too_many_requests.json'
+  'errors/500_internal_server_error.json'
 )
 
 baseDestination="./test/mocks"
