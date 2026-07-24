fe99c53242aa03b3bc3ebfedc78bcdb4f6e1b38e
Eray Aydın <eray.aydin@fingerprint.com>
2026-07-24 17:53:40 +0300

    chore: remove stale deprecation suppression
    The error_reporting suppression in sealed_results_example.php was added
    to silence ArrayAccess return type deprecation warnings. This is no
    longer needed.
    
    The comment also referenced swagger-codegen which is incorrect.
    
    Related-Task: INTER-2355

--- sealed_results_example.php
+++ sealed_results_example.php
@@ -12,11 +12,6 @@ $dotenv->safeLoad();
 $sealed_result = base64_decode($_ENV['BASE64_SEALED_RESULT'] ?? getenv('BASE64_SEALED_RESULT') ?? '');
 $sealed_key = base64_decode($_ENV['BASE64_KEY'] ?? getenv('BASE64_KEY') ?? '');
 
-// Temporarily suppress a million deprecated ArrayAccess return type warnings for readability
-// Our SDK generator does not yet support PHP's new attributes system
-// https://github.com/swagger-api/swagger-codegen/issues/11820
-error_reporting(error_reporting() & ~E_DEPRECATED);
-
 try {
     $data = Sealed::unsealEventResponse($sealed_result, [new DecryptionKey($sealed_key, DecryptionAlgorithm::AES_256_GCM)]);
 
@@ -26,9 +21,6 @@ try {
 
     exit(1);
 }
-
-// Enable the deprecated ArrayAccess return type warning again if needed
-error_reporting(error_reporting() | E_DEPRECATED);
 
 fwrite(STDOUT, "Checks passed\n");
 
